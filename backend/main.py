93aa7641323adbbace8a20a7537d71b377fe333a
ilmannk28 <ilmannk28@gmail.com>
2025-06-15 04:04:24 +0700

    menyetel port

--- backend/main.py
+++ backend/main.py
@@ -61,3 +61,9 @@ def get_articles():
     except Exception as e:
         return {"error": str(e)}
 
+if __name__ == "__main__":
+    import os
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 8000)) 
+    uvicorn.run(app, host="0.0.0.0", port=port)
